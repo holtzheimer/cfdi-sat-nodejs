5a6d5d19f1e6d8231399b506acb509df126c1c31
luisjossam <luisjossam@outlook.com>
2025-07-05 13:52:14 -0600

    Changes on gh-pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,5 @@
 export default {
-  base: "/cfdi-sat-nodejs",
+  base: "/cfdi-sat-nodejs/",
   title: "cfdi-sat-nodejs",
   description: "Generador CFDI 4.0, Carta Porte 3.1 y más en Node.js",
   themeConfig: {
